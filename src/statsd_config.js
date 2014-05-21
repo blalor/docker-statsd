d9f588c92c6406e347887975afe5fff8d42aaf90
Brian Lalor <blalor@bravo5.org>
2014-05-21 14:57:09 -0400

    Fix statsd config
    I *swear* I'd already committed this once.  Git sez otherwise, tho.

--- src/statsd_config.js
+++ src/statsd_config.js
@@ -99,8 +99,13 @@ Optional Variables:
 
 */
 {
-  graphitePort: 2003
-, graphiteHost: "graphite.example.com"
-, port: 8125
-, backends: [ "./backends/graphite" ]
+    graphitePort: process.env.GRAPHITE_PORT || 2003,
+    graphiteHost: process.env.GRAPHITE_HOST,
+    graphite: {
+        legacyNamespace: false,
+    },
+    port: 8125,
+    backends: [
+        "./backends/graphite"
+    ],
 }
